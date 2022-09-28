aaca13beae4f47940dfd4d444a74cd5d51fe0394
jwiz <jordanheath945@gmail.com>
2022-09-28 11:31:44 -0400

    added rows

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,6 +41,18 @@ const guessRows = [
     ['', '', '', '']
 ]
 
+guessRows.forEach((guessRow, guessRowIndex) => {
+    const rowElement = document.createElement('div')
+    rowElement.setAttribute('id', 'guessRow-' + guessRowIndex)
+    guessRow.forEach((guess, guessIndex) => {
+        const tileElement = document.createElement('div')
+        tileElement.setAttribute('id', 'guessRow-' + guessRowIndex + '-tile-' + guessIndex)
+        tileElement.classList.add('tile')
+        rowElement.append(tileElement)
+    })
+    tileDisplay.append(rowElement)
+})
+
 keys.forEach(key => {
     const buttonElement = document.createElement('button')
     buttonElement.textContent = key
